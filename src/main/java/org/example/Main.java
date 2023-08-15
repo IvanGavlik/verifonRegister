5ac203a738476361d375186ad948c8c062c2d3c3
ivan <ivangavlik963@gmail.com>
2023-08-15 22:51:30 +0200

    encode bytes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,26 @@ import java.net.*;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Random;
 
 public class Main {
+    static byte[] publicEncodedBytes;
+
+    static {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair keypair = keyGen.genKeyPair();
+            publicEncodedBytes = keypair.getPublic().getEncoded();
+        } catch (Exception exception) {
+            System.out.println("Start failed: " + exception);
+        }
+
+    }
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println("App start version 1.0.1");
         String host = args[0];
         int port = 5015;
 
@@ -16,14 +32,14 @@ public class Main {
         String entryCode = String.valueOf(generator.nextInt(9999));
 
         System.out.println("Entry code " + entryCode);
-
+        String publicEncodedString = Base64.getEncoder().encodeToString(publicEncodedBytes);
 
         String registrationMessage = "<TRANSACTION>\n" +
                 "  <FUNCTION_TYPE>SECURITY</FUNCTION_TYPE>\n" +
                 "  <COMMAND>REGISTER</COMMAND>\n" +
                 "  <ENTRY_CODE>"+ entryCode +"</ENTRY_CODE>\n" +
-                "  <KEY>Your_Public_Key_Here</KEY>\n" +
-                "  <REG_VER>2</REG_VER>\n" +
+                "  <KEY>"+publicEncodedString+"</KEY>\n" +
+                "  <REG_VER>1</REG_VER>\n" +
                 "</TRANSACTION>";
 
         try (Socket socket = new Socket(host, port);
