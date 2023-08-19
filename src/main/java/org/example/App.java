11bbf6279e1f1727080f11c0f3ae5aa705371c01
ivan <ivangavlik963@gmail.com>
2023-08-19 22:03:42 +0200

    refactor

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,269 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.function.Supplier;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App(args[0]);
+        app.unregister();
+        RegisterResponse registerResponse = app.register();
+        TestMacResponse testMacResponse = app.testMac(registerResponse);
+        System.out.println(testMacResponse);
+    }
+    private static final int PORT = 5015;
+    private final String HOST;
+    private Counter counterInstance = new Counter();
+
+    private App(String host) {
+        this.HOST = host;
+    }
+
+    public void unregister() {
+        XMLRepresentation unregisterRequest = new UnregisterRequest();
+        socketSend(HOST, PORT, unregisterRequest.get());
+    }
+    private RegisterResponse register() {
+        int entryCode = 1234;
+        byte[] key = {1};
+        XMLRepresentation registerRequest = new RegisterRequest(entryCode, key);
+        String response = socketSend(HOST, PORT, registerRequest.get());
+        return new RegisterResponse(response);
+    }
+
+    private TestMacResponse testMac(RegisterResponse registerResponse) {
+        // TODO MAC KEY
+        XMLRepresentation testMac = new TestMacRequest(registerResponse.getMacLabel(), "", counterInstance.getNext());
+        String response = socketSend(HOST, PORT, testMac.get());
+        return new TestMacResponse(response);
+    }
+    private static String socketSend(String host, int port, String message) {
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             Scanner sc = new Scanner(new InputStreamReader(socket.getInputStream()))) {
+
+            // Send registration message
+            out.println(message);
+
+            // Receive and process response
+            StringBuilder responseBuilder = new StringBuilder();
+            while (sc.hasNextLine()) {
+                String l = sc.nextLine();
+                System.out.println("Response from device: " + l);
+                // Process the response as needed
+                responseBuilder.append(l);
+                if (l.contains("</RESPONSE>")) {
+                    socket.close();
+                }
+                System.out.println("Response processing");
+            }
+            System.out.println("Response end");
+            return responseBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+interface XMLRepresentation extends Supplier<String> { }
+class UnregisterRequest implements XMLRepresentation {
+    private final String type = "SECURITY";
+    private final String command = "UNREGISTERALL";
+
+    @Override
+    public String get() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<TRANSACTION>")
+                .append("<FUNCTION_TYPE>").append(this.type).append("</FUNCTION_TYPE>")
+                .append("<COMMAND>").append(command).append("</COMMAND>")
+                .append("</TRANSACTION>");
+        return stringBuilder.toString();
+    }
+}
+class RegisterRequest implements XMLRepresentation {
+
+    private final String type = "SECURITY";
+    private final String command = "REGISTER";
+
+    private final int entryCode;
+
+    private final byte[] key;
+
+    private final int regVersion = 1;
+
+    public RegisterRequest(final int entryCode, final byte[] key) {
+        this.entryCode = entryCode;
+        this.key = key;
+    }
+
+    @Override
+    public String get() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<TRANSACTION>")
+                .append("<FUNCTION_TYPE>").append(type).append("</FUNCTION_TYPE>")
+                .append("<COMMAND>").append(command).append("</COMMAND>")
+                .append("<ENTRY_CODE>").append(entryCode).append("</ENTRY_CODE>")
+                .append("<KEY>").append(key).append("</KEY>")
+                .append("<REG_VER>").append(regVersion).append("</REG_VER>")
+                .append("</TRANSACTION>");
+        return stringBuilder.toString();
+    }
+}
+class TestMacRequest implements XMLRepresentation {
+    private final String type = "SECURITY";
+    private final String command = "TEST_MAC";
+    private final String macLabel;
+    private final String mac;
+
+    private final long counter;
+
+    public TestMacRequest(final String macLabel, final String mac, final long counter) {
+        this.macLabel = macLabel;
+        this.mac = mac;
+        this.counter = counter;
+    }
+    @Override
+    public String get() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<TRANSACTION>")
+                .append("<FUNCTION_TYPE>").append(type).append("</FUNCTION_TYPE>")
+                .append("<COMMAND>").append(command).append("</COMMAND>")
+                .append("<MAC_LABEL>").append(macLabel).append("</MAC_LABEL>")
+                .append("<MAC>").append(mac).append("</MAC>")
+                .append("<COUNTER>").append(counter).append("</COUNTER>")
+                .append("</TRANSACTION>");
+        return stringBuilder.toString();
+    }
+}
+abstract class XMLResponse {
+    protected XMLResponse(final String response) {
+        this.constructObjectFromResponse(response);
+    }
+    abstract void constructObjectFromResponse(final String response);
+
+    protected String getValueFromXMLElement(final String element, final String xml) {
+        int start = xml.indexOf("<"+element+">");
+        int end = xml.indexOf("</"+element+">");
+        return xml.substring(start, end).replace("<"+element+">", "").trim();
+    }
+}
+class RegisterResponse extends XMLResponse {
+    private String responseText;
+    private String result;
+    private String resultCode;
+    private String terminationStatus;
+    private String macKey;
+    private String macLabel;
+    private String entryCode;
+
+    protected RegisterResponse(final String response) {
+        super(response);
+    }
+
+    @Override
+    void constructObjectFromResponse(final String response) {
+        this.responseText = getValueFromXMLElement("RESPONSE_TEXT", response);
+        this.result = getValueFromXMLElement("RESULT", response);
+        this.resultCode = getValueFromXMLElement("RESULT_CODE", response);
+        this.terminationStatus = getValueFromXMLElement("TERMINATION_STATUS", response);
+        this.macKey = getValueFromXMLElement("MAC_KEY", response);
+        this.macLabel = getValueFromXMLElement("MAC_LABEL", response);
+        this.entryCode = getValueFromXMLElement("ENTRY_CODE", response);
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getTerminationStatus() {
+        return terminationStatus;
+    }
+
+    public String getMacKey() {
+        return macKey;
+    }
+
+    public String getMacLabel() {
+        return macLabel;
+    }
+
+    public String getEntryCode() {
+        return entryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterResponse{" +
+                "responseText='" + responseText + '\'' +
+                ", result='" + result + '\'' +
+                ", resultCode='" + resultCode + '\'' +
+                ", terminationStatus='" + terminationStatus + '\'' +
+                ", macKey='" + macKey + '\'' +
+                ", macLabel='" + macLabel + '\'' +
+                ", entryCode='" + entryCode + '\'' +
+                '}';
+    }
+}
+class TestMacResponse extends XMLResponse {
+
+    private String responseText;
+    private String result;
+    private String resultCode;
+    private String terminationStatus;
+    private String counter;
+    public TestMacResponse(final String response) {
+        super(response);
+    }
+
+    @Override
+    void constructObjectFromResponse(String response) {
+        this.responseText = getValueFromXMLElement("RESPONSE_TEXT", response);
+        this.result = getValueFromXMLElement("RESULT", response);
+        this.resultCode = getValueFromXMLElement("RESULT_CODE", response);
+        this.terminationStatus = getValueFromXMLElement("TERMINATION_STATUS", response);
+        this.counter = getValueFromXMLElement("COUNTER", response);
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getTerminationStatus() {
+        return terminationStatus;
+    }
+
+    public String getCounter() {
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        return "TestMacResponse{" +
+                "responseText='" + responseText + '\'' +
+                ", result='" + result + '\'' +
+                ", resultCode='" + resultCode + '\'' +
+                ", terminationStatus='" + terminationStatus + '\'' +
+                ", counter='" + counter + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/Terminal.java
+++ src/main/java/org/example/Terminal.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import javax.crypto.Cipher;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.util.Arrays;
 import java.util.Base64;
-import javax.crypto.Mac;
 
 public class Terminal {
     static byte[] publicEncodedBytes;
@@ -79,4 +75,94 @@ public class Terminal {
                 ", lastResponse='" + lastResponse + '\'' +
                 '}';
     }
+
+
+
+    private static final String ALGORITHM = "AES";
+    private static final byte[] keyValue = "ADBSJHJS12547896".getBytes();
+
+    public static void main(String args[]) throws Exception {
+        String password=encrypt("password123");
+        System.out.println(password);
+        System.out.println(decrypt(password));
+
+    }
+
+    static  byte[]  key = "!@#$!@#$%^&**&^%".getBytes();
+    final static String algorithm="AES";
+
+    public static String encrypt(String data){
+
+        byte[] dataToSend = data.getBytes();
+        Cipher c = null;
+        try {
+            c = Cipher.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        SecretKeySpec k =  new SecretKeySpec(key, algorithm);
+        try {
+            c.init(Cipher.ENCRYPT_MODE, k);
+        } catch (InvalidKeyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        byte[] encryptedData = "".getBytes();
+        try {
+            encryptedData = c.doFinal(dataToSend);
+        } catch (IllegalBlockSizeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        byte[] encryptedByteValue =    Base64.getEncoder().encode(encryptedData);
+        return  new String(encryptedByteValue);//.toString();
+    }
+
+    public static String decrypt(String data){
+
+        byte[] encryptedData  = Base64.getDecoder().decode(data);
+        Cipher c = null;
+        try {
+            c = Cipher.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        SecretKeySpec k =
+                new SecretKeySpec(key, algorithm);
+        try {
+            c.init(Cipher.DECRYPT_MODE, k);
+        } catch (InvalidKeyException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        byte[] decrypted = null;
+        try {
+            decrypted = c.doFinal(encryptedData);
+        } catch (IllegalBlockSizeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new String(decrypted);
+    }
+
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+        return key;
+    }
+
+
 }
